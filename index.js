8efb7e9d5c00a6fb77af1cd9f9848d3288604149
bobomurod <bobomurod.boboev@yandex.ru>
2018-07-10 23:34:55 +0500

    database connection and testing done

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ var connection = mysql.createConnection({
     host        :   'localhost',
     user        :   'root',
     password    :   '',
-    database    :   'uproject'
+    database    :   'test'
 })
 
-connection.connect()
-connection.query('SELECT * FROM prices', function(error, result, fields) {
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connection', err.stack)
+        return
+    }
+    console.log('Client connected', connection.threadId)
+})
+connection.query('SELECT * FROM prices', function(error, results, fields) {
     if (error) throw error;
-    console.log(result[0].solution)
+    for (var n in results) {
+        console.log(results[n])
+    }
 });
 
 
 
+connection.end();
